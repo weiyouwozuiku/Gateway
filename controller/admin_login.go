77d17d25a947122a8683bfc69fc796f7233c7711
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2022-08-31 09:46:50 +0800

    update

--- controller/admin_login.go
+++ controller/admin_login.go
@@ -1,12 +1,1 @@
 package controller
-
-import "github.com/gin-gonic/gin"
-
-type AdminLoginInput struct {
-	Username string `json:"username" form:"username" comment:"用户名" example:"admin" validate:"required"`
-	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`
-}
-
-func (input *AdminLoginInput) Bind(c *gin.Context) error {
-	return c.ShouldBind(input)
-}
