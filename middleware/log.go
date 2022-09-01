24c4e63e51528afb608d7e7553aa28d062e0ee26
robertwang <19681022+weiyouwozuiku@users.noreply.github.com>
2022-09-01 21:44:21 +0800

    save

--- middleware/log.go
+++ middleware/log.go
@@ -1,5 +1,11 @@
 package middleware
 
+import "net"
+
+var TimeFormat = "2022-09-01 00:00:00"
+var DataFormat = "2022-09-01"
+var LocalIP = net.ParseIP("127.0.0.1")
+
 type Trace struct {
 	TraceId     string
 	SpanId      string
--- middleware/mysql.go
+++ middleware/mysql.go
@@ -3,13 +3,49 @@ package middleware
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func InitDBPool(path string) error {
-	//普通DB方式
-	DbConfMap := &MysqlMapConf{}
-	err := ParseConfig(path, DbConfMap)
+	DBConfMap := &MysqlMapConf{}
+	err := ParseConfig(path, DBConfMap)
+	if err != nil {
+		return err
+	}
+	if len(DBConfMap.List) == 0 {
+		fmt.Printf("[info] %s%s\n", time.Now().Format(TimeFormat), " empty mysql config.")
+	}
+	DBMapPool = map[string]*sql.DB{}
+	GORMMapPool = map[string]*gorm.DB{}
+	for confName, DBConf := range DBConfMap.List {
+		dbpool, err := sql.Open("mysql", DBConf.DataSourceName)
+		if err != nil {
+			return err
+		}
+		dbpool.SetMaxOpenConns(DBConf.MaxOpenConn)
+		dbpool.SetMaxIdleConns(DBConf.MaxIdleConn)
+		dbpool.SetConnMaxLifetime(time.Duration(DBConf.MaxConnLifeTime) * time.Second)
+		err = dbpool.Ping()
+		if err != nil {
+			return err
+		}
+		dbgorm, err := gorm.Open(mysql.Open(DBConf.DataSourceName), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true},
+				Logger: ,
+		})
+		db, _ := dbgorm.DB()
+		err = db.Ping()
+		if err != nil {
+			return err
+		}
+
+	}
 }
 
 func GetDBPool(name string) (*sql.DB, error) {
@@ -24,4 +60,8 @@ func GetGormPool(name string) (*gorm.DB, error) {
 		return dbpool, nil
 	}
 	return nil, errors.New("get db pool error")
+}
+
+type MySQLGormLogger struct {
+	
 }
