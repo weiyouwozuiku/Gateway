88fc877b0dc9986e2c19c5b965e950e086405e23
robertwang <19681022+weiyouwozuiku@users.noreply.github.com>
2022-09-03 03:12:43 +0800

    save

--- router/httpserver.go
+++ router/httpserver.go
@@ -1,12 +1,15 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/weiyouwozuiku/gateway/middleware"
 )
 
 var HttpSrvHandler *http.Server
 
 func HttpServerRun() {
-	gin.SetMode(Get)
+	gin.SetMode(middleware.GetStringConf("base.base.debug_mode"))
+
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,7 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+func InitRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
+
+}
