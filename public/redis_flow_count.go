d501b6144b4a1f073b76cab7191923863fe7fcb5
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2023-04-25 10:05:57 +0800

    save

--- public/redis_flow_count.go
+++ public/redis_flow_count.go
@@ -1,6 +1,10 @@
 package public
 
-import "time"
+import (
+	"github.com/weiyouwozuiku/Gateway/log"
+	"sync/atomic"
+	"time"
+)
 
 type RedisFlowCountService struct {
 	AppID       string
@@ -11,17 +15,29 @@ type RedisFlowCountService struct {
 	TotalCount  int64
 }
 
+func (s *RedisFlowCountService) GetDayKey(t *time.Time) string {
+	dayStr := t.In(TimeLocation)
+}
+
 func NewRedisFlowCountService(appID string, interval time.Duration) *RedisFlowCountService {
-	service := &RedisFlowCountService{
+	reqCounter := &RedisFlowCountService{
 		AppID:    appID,
 		Interval: interval,
 	}
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
-
+				log.Error("NewRedisFlowCountService error||error=%v", err)
 			}
 		}()
+		ticker := time.NewTicker(interval)
+		for {
+			<-ticker.C
+			tickerCount := atomic.LoadInt64(&reqCounter.TickerCount) // 获取数据
+			atomic.StoreInt64(&reqCounter.TickerCount, 0)            //重置数据
+			currentTime := time.Now()
+			dayKey := reqCounter.Get
+		}
 	}()
 	return service
 }
